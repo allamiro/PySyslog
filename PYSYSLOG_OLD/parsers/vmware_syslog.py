7d42a1bfa71a66ddc413c6c9078ca87c32e56805
Tamir Suliman <allamiro@gmail.com>
2024-12-12 16:41:41 +0100

    Update vmware_syslog.py

--- PYSYSLOG_OLD/parsers/vmware_syslog.py
+++ PYSYSLOG_OLD/parsers/vmware_syslog.py
@@ -1,1 +1,26 @@
+# Import python libraries needed 
+import re
+
+def vmware_syslog(log):
+  
+  cef_prefix = 'CEF:0|VM|VM|1.0||SYSLOG|Low|'  # << need to check the CEF guide for arcsight on how they doing this 
+  log_patterns = [
+    {
+      'pattern' : r"",
+      'fields'  : []
+    }
+ 
+  ]
+  field_mapping = {
+    
+  }
+  for pattern_info in log_patterns:
+    pattern = pattern_info['pattern']
+    fields = pattern_info['fields']
+    match = re.search(pattern, log)
+      if match:
+        cef_values = ' '.join(f"")
+        cef_log = 
+        return cef_log
+  return None
 
