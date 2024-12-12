4d91c3538a8cd3937f3ead86b4ebed94b2e264ec
Tamir Suliman <allamiro@gmail.com>
2024-12-12 16:28:27 +0100

    Update vmware.py

--- PYSYSLOG_OLD/vmware.py
+++ PYSYSLOG_OLD/vmware.py
@@ -10,3 +10,12 @@ import time
 import codecs
 import logging.handlers
 
+# Change the root logger level 
+logging.getlogger('').setlevel(logging.NOTSET)
+# Import my parsers
+from parsers import vmware_syslog
+# Setup logging
+logging.basicConfig(filename='logs/vmware.log', level=logging.INFO, format='',datefmt='%Y-%m-%d %H:%M')
+
+handler = logging.handlers.RotatingFileHandler(filename='logs/vmware.log', maxBytes=20000, backupCount=2)
+
