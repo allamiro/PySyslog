361c4d46a7f04156765485a1caf48040dc8feb3e
Tamir Suliman <allamiro@gmail.com>
2024-12-12 17:04:27 +0100

    Update vmware_syslog.py

--- PYSYSLOG_OLD/parsers/vmware_syslog.py
+++ PYSYSLOG_OLD/parsers/vmware_syslog.py
@@ -1,30 +1,33 @@
-# Import python libraries needed 
+# Import Python libraries needed
 import re
 
 def vmware_syslog(log):
-  
-  cef_prefix = 'CEF:0|VM|VM|1.0||SYSLOG|Low|'  # << need to check the CEF guide for arcsight on how they doing this 
-  log_patterns = [
-    {
-      'pattern' : r"(\d+-\d+-\d+w+\d+:\d+.\d+\+\d+:\d+) (\d+.\d+.\d+.\d+)(.*)",
-      'fields'  : [ 'timestamp', 'hostname', 'message'],
-    }
- 
-  ]
-  # This is where i match the CEF fields with the pattern extracted from above
-  field_mapping = {
-    'priority' : 'cs1',
-    'severity' : 'sev',
-    'timestamp' : 'end',
-    'hostname'  : 'dvchost',
-  }
-  for pattern_info in log_patterns:
-    pattern = pattern_info['pattern']
-    fields = pattern_info['fields']
-    match = re.search(pattern, log)
-      if match:
-        cef_values = ' '.join(f"{field_mapping[field]}={match.group(index)}" for index, field in enumerate(fields, start=1))
-        cef_log = f'{cef_prefix}{cef_values}'
-        return cef_log
-  return None
+    cef_prefix = 'CEF:0|VM|VM|1.0||SYSLOG|Low|'  # << need to check the CEF guide for ArcSight
+    log_patterns = [
+        {
+            'pattern': r"(\d+-\d+-\d+w+\d+:\d+.\d+\+\d+:\d+) (\d+.\d+.\d+.\d+)(.*)",
+            'fields': ['timestamp', 'hostname', 'message'],
+        }
+    ]
 
+    # This is where I match the CEF fields with the pattern extracted from above
+    field_mapping = {
+        'priority': 'cs1',
+        'severity': 'sev',
+        'timestamp': 'end',
+        'hostname': 'dvchost',
+    }
+
+    for pattern_info in log_patterns:
+        pattern = pattern_info['pattern']
+        fields = pattern_info['fields']
+        match = re.search(pattern, log)
+        if match:  # Correctly indented now
+            cef_values = ' '.join(
+                f"{field_mapping[field]}={match.group(index)}"
+                for index, field in enumerate(fields, start=1)
+            )
+            cef_log = f'{cef_prefix}{cef_values}'
+            return cef_log
+
+    return None
